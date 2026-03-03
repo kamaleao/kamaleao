af253ecca3323f7e54f0b4c27fc44b9c967e497c
Kamaleao <petrus@kamaleao.com>
2026-03-03 00:37:08 -0300

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // ou seu plugin (vue, etc)
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], // mantenha seus plugins se já existirem
+  plugins: [react()],
   server: {
     allowedHosts: [
       'devserver-preview--kamaleao.netlify.app'
